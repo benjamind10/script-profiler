1d33cafe69c99c511df26c4a55da431ccd683192
Ben Duran <benjamind10@pm.me>
2025-07-01 17:13:15 -0400

    update to check rcp

--- designer/src/main/java/com/shiva/designer/ScriptProfilerDesignerHook.java
+++ designer/src/main/java/com/shiva/designer/ScriptProfilerDesignerHook.java
@@ -1,28 +1,23 @@
 package com.shiva.designer;
 
+import com.inductiveautomation.ignition.common.licensing.LicenseState;
 import com.inductiveautomation.ignition.common.util.LogUtil;
 import com.inductiveautomation.ignition.common.util.LoggerEx;
 import com.inductiveautomation.ignition.designer.model.AbstractDesignerModuleHook;
 import com.inductiveautomation.ignition.designer.model.DesignerContext;
-import com.inductiveautomation.ignition.common.licensing.LicenseState;
 
 public class ScriptProfilerDesignerHook extends AbstractDesignerModuleHook {
 
     private final LoggerEx log = LogUtil.getLogger(getClass().getSimpleName());
 
-    /**
-     * Called once licensing has been checked in the Designer.
-     */
     @Override
-    public void startup(DesignerContext context, LicenseState licenseState) throws Exception {
-        log.info("Script Profiler designer startup (license = {})", (Throwable) licenseState);
+    public void startup(DesignerContext context, LicenseState licenseState) {
+        log.info("Script Profiler Designer startup. License active: " + licenseState.toString());
+        // You can also inspect licenseState.getModuleId() etc.
     }
 
-    /**
-     * Called when the Designer is closing or the module is unloaded.
-     */
     @Override
     public void shutdown() {
-        log.info("Script Profiler designer shutdown");
+        log.info("Script Profiler Designer shutdown");
     }
 }
